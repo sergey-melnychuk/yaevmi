60d07ec1b51edbf3d68eb03a4c823c8b26dd06f4
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2026-03-15 22:13:45 +0100

    wasm: setup sub-project

--- yaevmi-wasm/src/lib.rs
+++ yaevmi-wasm/src/lib.rs
@@ -1,1 +1,47 @@
-// TODO: wasm-bindgen functions and objects
+#[cfg(target_arch = "wasm32")] // comment-out this line for development
+mod wasm {
+    use js_sys::JsString;
+    use wasm_bindgen::prelude::*;
+    use yaevmi_core::Call;
+
+    #[wasm_bindgen]
+    pub fn hello(name: JsString) -> JsString {
+        JsString::from(format!("Hello, {name}").as_str())
+    }
+
+    #[wasm_bindgen]
+    pub fn call(json: JsValue) -> Result<JsValue, JsError> {
+        let call: Call = serde_wasm_bindgen::from_value(json)?;
+        Ok(serde_wasm_bindgen::to_value(&call)?)
+    }
+
+    // TODO: YAVMI: Executor, Call & Builder, Int/Acc/Buf, Tx/Head
+    /*
+    ```javascript
+    let call = {
+        by: '0x1',
+        to: '0x2',
+        eth: '0x3',
+        gas: 4,
+        data: '0x5'
+    };
+    let exe = new Executor(call);
+
+    let tx = {
+        // ...
+    };
+    let head = {
+        // ...
+    };
+    let state = new Cache();
+    let chain = new Rpc(URL);
+
+    let res = await exe.run(head, tx, state, chain);
+    // res.state - object with touched state
+    // res.steps - array of trace objects
+    ```
+    */
+}
+
+#[cfg(target_arch = "wasm32")]
+pub use wasm::*;
